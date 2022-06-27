92bb04621eab7a8b76da3ec54a61453202be4314
yillkid <yillkid@gmail.com>
2022-06-27 15:34:16 +0800

    feat: add JSON parse to response

--- docs/static/js/action.js
+++ docs/static/js/action.js
@@ -9,11 +9,23 @@ function submit_data() {
   // Submit
   var request = new XMLHttpRequest();
   request.onreadystatechange= function () {
-    if (request.readyState==4) {
+    if (request.readyState == 4) {
       // handle response
       // alert(request.responseText);
       console.log(request.responseText);
-      document.getElementById("responseText").innerHTML = request.responseText;
+      // document.getElementById("responseText").innerHTML = request.responseText;
+
+      // Process
+      obj_response = JSON.parse(request.responseText);
+      message_id = obj_response.messageID
+      explorer = obj_response.explorer
+
+      output = "<p> message ID : " + message_id + "</p>";
+      output = output + "<a href = " + explorer + "/>" + message_id + "</a></p>"
+      document.getElementById("responseText").innerHTML = output;
+
+    } else {
+      document.getElementById("responseText").innerHTML = "<p> system processing... </p>"
     }
   }
   request.open("POST", "https://poe.townway.com.tw/iota/message", true);
